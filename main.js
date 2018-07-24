ea564ba8fd2f52c7752f27a99c2efd2a6d8c1ddb
paulcysu <su.paul.cy@gmail.com>
2018-07-24 10:04:16 -0700

    making button to next page

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
-function name (hello) {
-    
+$(document).ready(initializeApp);
+
+let foodInput = null;
+
+function initializeApp () {
+    addClickHandler;
+}
+
+function addClickHandler () {
+    $(".submit").click(submitClicked);
+}
+
+function submitClicked () {
+    retrieveInput();
+    location.href = "www.yoursite.com"
+}
+
+function retrieveInput () {
+    foodInput = $("#foodInput").value();
 }
