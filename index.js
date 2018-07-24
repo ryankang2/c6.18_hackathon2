dd3d924da427599572d43c8f349bfc1119b950f0
Ryan Kang <shk130@ucsd.edu>
2018-07-24 12:18:18 -0700

    twitter gone

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-$(document).ready(initializeApp);
+// $(document).ready(initializeApp);
 
-let foodInput = null;
+// let foodInput = null;
 
-/**
- * @param {}
- */
-function initializeApp () {
-    addClickHandler();
-}
+// /**
+//  * @param {}
+//  */
+// function initializeApp () {
+//     addClickHandler();
+// }
 
-/**
- * @param  {}
- * @param  {}
- */
-function addClickHandler () {
-    $(".submit").click(submitClicked);
-}
+// /**
+//  * @param  {}
+//  * @param  {}
+//  */
+// function addClickHandler () {
+//     $(".submit").click(submitClicked);
+// }
 
-/**
- * @param  {}
- */
-function submitClicked () {
-    debugger;
-    retrieveInput();
-    changePage();
-}
+// /**
+//  * @param  {}
+//  */
+// function submitClicked () {
+//     debugger;
+//     retrieveInput();
+//     changePage();
+// }
 
-function changePage () {
-    location.assign("food.html")
-}
+// function changePage () {
+//     location.assign("food.html")
+// }
 
-function retrieveInput () {
-    foodInput = $("#foodInput").val();
-}
+// function retrieveInput () {
+//     foodInput = $("#foodInput").val();
+// }
