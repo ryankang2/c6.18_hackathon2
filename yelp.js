6d083a1c43882bfd7429e730b83c15f2ef661750
paulcysu <su.paul.cy@gmail.com>
2018-07-24 12:20:10 -0700

    added yelp

--- yelp.js
+++ yelp.js
@@ -1,28 +1,12 @@
-// change googleIndex name
-$(document).ready(food.html);
+// $(document).ready(initializeApp);
 
-// var settings = {
-//     "url": "https://yelp.ongandy.com/businesses",
-//     "method": "POST",
-//     "dataType": "JSON",
-//     "data": {
-//         term: "mexican",
-//         location: "irvine", 
-//         api_key: "XSyryzoREYThrY1P0pDAkbK9uJV0j7TVklsKegO9g9aqqqGz87SZPuhQ0Cob0jzZ6G1BCVE9JaycPHyB2OI7hXgTJYs_enS7SKr1G21Jf45cDBYbUAHOFnh-r3FWW3Yx"
-//     },
-//     success: function(response) {
-//         console.log(response);
-//     },
-//     error: function(err) {
-//         console.log(err);
-//     }
-// }
-
-// $.ajax(settings).done(function (response) {
-//     console.log(response);
-// });
-
-function requestYelpData (keywordOrAddress = irvine, location = irvine, latitude, longitude) {
+/**
+ * @param  {keywordOfAddress, location}
+ * @return {list of resturants}
+ * Function that pulls yelp API with keyword/address search and current location (city)
+ */
+function requestYelpData (keywordOrAddress = irvine, location = irvine) {
+    debugger;
     let key = {
         api_key: "XSyryzoREYThrY1P0pDAkbK9uJV0j7TVklsKegO9g9aqqqGz87SZPuhQ0Cob0jzZ6G1BCVE9JaycPHyB2OI7hXgTJYs_enS7SKr1G21Jf45cDBYbUAHOFnh-r3FWW3Yx",
         term: keywordOrAddress,
