355115fe1f41dc322c5d68f65a358fa0f2abb886
John Carlisle <jmcarlisle@live.com>
2018-07-24 18:37:13 -0700

    removed duplicate requestYelpData function

--- food.js
+++ food.js
@@ -179,30 +179,30 @@ function populateAddressInfo( string ) {
 
 }
 
-function requestYelpData (name, address, city) {
-    let customUrl = "https://yelp.ongandy.com/businesses/matches";
-    let key = {
-        api_key: "9bPpnQ55-8I0jLR62WqbyvBAv20IJ-zF-WJs7YJgLqZeRqokQg2L995TrDHKUVXEmRblz6We2EMClsxkS4vbfmRLLP5G1cPcV5FFX0fzSi388ha6a1qsHR5J97dWW3Yx",
-        name: name,
-        address1: address,
-        city: city,
-        state: "CA",
-        country: "US",
-      }
-    let yelpAPI = {
-        data: key,
-        url: customUrl,
-        method: "POST",
-        dataType: "json",
-        success: function (response) {
-            console.log(response);
-            var businessId= response.businesses[0].id;
-            getYelpDetails(businessId);
-        },
-        error: function () {
-            console.log("fail")
-        }
+// function requestYelpData (name, address, city) {
+//     let customUrl = "https://yelp.ongandy.com/businesses/matches";
+//     let key = {
+//         api_key: "9bPpnQ55-8I0jLR62WqbyvBAv20IJ-zF-WJs7YJgLqZeRqokQg2L995TrDHKUVXEmRblz6We2EMClsxkS4vbfmRLLP5G1cPcV5FFX0fzSi388ha6a1qsHR5J97dWW3Yx",
+//         name: name,
+//         address1: address,
+//         city: city,
+//         state: "CA",
+//         country: "US",
+//       }
+//     let yelpAPI = {
+//         data: key,
+//         url: customUrl,
+//         method: "POST",
+//         dataType: "json",
+//         success: function (response) {
+//             console.log(response);
+//             var businessId= response.businesses[0].id;
+//             getYelpDetails(businessId);
+//         },
+//         error: function () {
+//             console.log("fail")
+//         }
 
-    }
-    $.ajax(yelpAPI)
-}
+//     }
+//     $.ajax(yelpAPI)
+// }
